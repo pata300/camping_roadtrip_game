771dd04761a4fe3ac0a03a9cf4d496d156b248c4
pata300 <pat_rick_griffin@hotmail.com>
2018-03-04 13:27:38 -0600

    first commit of files

---
+++ RoadTrip/RVsim_prototype/Assets/Scripts/RotateWrench.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWrench : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//get the position of the cursor
+		Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 10);
+
+		transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+
+		Debug.Log("New wrench");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
